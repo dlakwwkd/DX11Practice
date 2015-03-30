3b6f4ffe147e33a2356e4df91d225a1498f8c036
Subin Park <dlakek@naver.com>
2015-03-31 02:05:11 +0900

    박스 만들기

---
+++ DX11Project2/WinMain.cpp
@@ -0,0 +1,552 @@
+#include <windows.h>
+#include <dxerr.h>
+#include <dxgi.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dCompiler.h>
+#include <xnamath.h>
+
+#pragma comment( lib, "dxguid.lib" )
+#pragma comment( lib, "dxerr.lib" )
+#pragma comment( lib, "dxgi.lib" )
+#pragma comment( lib, "d3d11.lib" )
+#pragma comment( lib, "d3dx11d.lib" )
+#pragma comment( lib, "d3dcompiler.lib" )
+
+#define szWindowClass	TEXT("BoxRender")
+#define szTitle			TEXT("BoxRender")
+
+
+//--------------------------------------------------------------------------------------
+// 구조체
+//--------------------------------------------------------------------------------------
+struct SimpleVertex
+{
+    XMFLOAT3 Pos;
+    XMFLOAT4 Color;
+};
+
+struct ConstantBuffer
+{
+    XMMATRIX mWorld;
+    XMMATRIX mView;
+    XMMATRIX mProjection;
+};
+
+
+//--------------------------------------------------------------------------------------
+// 전역 변수
+//--------------------------------------------------------------------------------------
+HINSTANCE               g_hInst = NULL;
+HWND                    g_hWnd = NULL;
+D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*           g_pd3dDevice = NULL;
+ID3D11DeviceContext*    g_pImmediateContext = NULL;
+IDXGISwapChain*         g_pSwapChain = NULL;
+ID3D11RenderTargetView* g_pRenderTargetView = NULL;
+ID3D11Texture2D*        g_pDepthStencil = NULL;
+ID3D11DepthStencilView* g_pDepthStencilView = NULL;
+ID3D11VertexShader*     g_pVertexShader = NULL;
+ID3D11PixelShader*      g_pPixelShader = NULL;
+ID3D11InputLayout*      g_pVertexLayout = NULL;
+ID3D11Buffer*           g_pVertexBuffer = NULL;
+ID3D11Buffer*           g_pIndexBuffer = NULL;
+ID3D11Buffer*           g_pConstantBuffer = NULL;
+XMMATRIX                g_World1;
+XMMATRIX                g_World2;
+XMMATRIX                g_View;
+XMMATRIX                g_Projection;
+
+
+//--------------------------------------------------------------------------------------
+// 전방 선언
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK        WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+HRESULT                 InitWindow(HINSTANCE hInstance, int nCmdShow);
+HRESULT                 InitDevice();
+void                    CleanupDevice();
+void                    Render();
+
+
+//--------------------------------------------------------------------------------------
+// 메인
+//--------------------------------------------------------------------------------------
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpszCmdParam);
+
+    if (FAILED(InitWindow(hInstance, nCmdShow)))
+        return 0;
+
+    if (FAILED(InitDevice()))
+    {
+        CleanupDevice();
+        return 0;
+    }
+
+    MSG	msg = { 0 };
+    while (WM_QUIT != msg.message)
+	{
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            Render();
+        }
+	}
+    CleanupDevice();
+	return (int)msg.wParam;
+}
+
+
+//--------------------------------------------------------------------------------------
+// 메시지 처리
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// 윈도우 초기화
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+    // Register class
+	WNDCLASSEX wcex;
+	wcex.cbSize		    = sizeof(WNDCLASSEX);
+	wcex.style		    = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon	        = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hIconSm        = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= szWindowClass;
+    if (!RegisterClassEx(&wcex))
+        return E_FAIL;
+
+    // Create window
+    g_hInst = hInstance;
+    g_hWnd = CreateWindowEx(WS_EX_APPWINDOW
+        , szWindowClass
+        , szTitle
+        , WS_OVERLAPPEDWINDOW
+        , CW_USEDEFAULT
+        , CW_USEDEFAULT
+        , 800
+        , 600
+        , NULL
+        , NULL
+        , hInstance
+        , NULL);
+    if (!g_hWnd)
+        return E_FAIL;
+
+    ShowWindow(g_hWnd, nCmdShow);
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Helper for compiling shaders with D3DX11
+//--------------------------------------------------------------------------------------
+HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+    HRESULT hr = S_OK;
+
+    DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+    // Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+    // Setting this flag improves the shader debugging experience, but still allows 
+    // the shaders to be optimized and to run exactly the way they will run in 
+    // the release configuration of this program.
+    dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+    ID3DBlob* pErrorBlob;
+    hr = D3DX11CompileFromFile(szFileName, NULL, NULL, szEntryPoint, szShaderModel,
+        dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL);
+    if (FAILED(hr))
+    {
+        if (pErrorBlob != NULL)
+            OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+        if (pErrorBlob) pErrorBlob->Release();
+        return hr;
+    }
+    if (pErrorBlob) pErrorBlob->Release();
+
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create Direct3D device and swap chain
+//--------------------------------------------------------------------------------------
+HRESULT InitDevice()
+{
+    HRESULT hr = S_OK;
+
+    RECT rc;
+    GetClientRect(g_hWnd, &rc);
+    UINT width = rc.right - rc.left;
+    UINT height = rc.bottom - rc.top;
+
+    // Flag 설정
+    UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+    createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+    D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0,
+    };
+    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = width;
+    sd.BufferDesc.Height = height;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// 백버퍼 포맷
+    sd.BufferDesc.RefreshRate.Numerator = 60; 	        // 분자(FPS)
+    sd.BufferDesc.RefreshRate.Denominator = 1; 	        // 분모(FPS)
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	// 백버퍼 렌더링
+    sd.OutputWindow = g_hWnd;		                    // 현재 윈도우
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+
+
+    hr = D3D11CreateDeviceAndSwapChain(
+        0,  		                // 기본 디스플레이 어댑터 사용
+        D3D_DRIVER_TYPE_HARDWARE, 	// 3D 하드웨어 가속
+        0,  		                // 소프트웨어 구동 안함
+        createDeviceFlags,
+        featureLevels,
+        numFeatureLevels,
+        D3D11_SDK_VERSION,          // SDK version
+        &sd,                        // Swap chain description
+        &g_pSwapChain,
+        &g_pd3dDevice,
+        &g_featureLevel,
+        &g_pImmediateContext);
+    if (FAILED(hr))
+        return hr;
+
+
+    // Create a render target view
+    ID3D11Texture2D* pBackBuffer = NULL;
+    hr = g_pSwapChain->GetBuffer(
+        0,                          // 후면 버퍼 색인. 여러개일때 중요. 지금은 1개이므로 0.
+        __uuidof(ID3D11Texture2D),  // 버퍼 형식
+        (LPVOID*)&pBackBuffer);     // 받아온 버퍼
+    if (FAILED(hr))
+        return hr;
+
+    hr = g_pd3dDevice->CreateRenderTargetView(
+        pBackBuffer,
+        NULL, 		            // 자원 형식 
+        &g_pRenderTargetView);
+    pBackBuffer->Release();		// Get을 해왔으면 반드시 Release
+    if (FAILED(hr))
+        return hr;
+
+
+    // Create depth stencil texture
+    D3D11_TEXTURE2D_DESC descDepth;
+    ZeroMemory(&descDepth, sizeof(descDepth));
+    descDepth.Width = width;
+    descDepth.Height = height;
+    descDepth.MipLevels = 1;
+    descDepth.ArraySize = 1;
+    descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    descDepth.SampleDesc.Count = 1;
+    descDepth.SampleDesc.Quality = 0;
+    descDepth.Usage = D3D11_USAGE_DEFAULT;
+    descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    descDepth.CPUAccessFlags = 0;
+    descDepth.MiscFlags = 0;
+    hr = g_pd3dDevice->CreateTexture2D(&descDepth, NULL, &g_pDepthStencil);
+    if (FAILED(hr))
+        return hr;
+
+    // Create the depth stencil view
+    D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+    ZeroMemory(&descDSV, sizeof(descDSV));
+    descDSV.Format = descDepth.Format;
+    descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    descDSV.Texture2D.MipSlice = 0;
+    hr = g_pd3dDevice->CreateDepthStencilView(g_pDepthStencil, &descDSV, &g_pDepthStencilView);
+    if (FAILED(hr))
+        return hr;
+
+
+    g_pImmediateContext->OMSetRenderTargets(
+        1,    	                // 렌더 대상 개수. 장면 분할시 1 초과
+        &g_pRenderTargetView,  	// 렌더 타겟
+        g_pDepthStencilView);	// 깊이/스텐실 버퍼
+
+
+    // Setup the viewport
+    D3D11_VIEWPORT vp;
+    vp.Width = (FLOAT)width;    // 뷰포트 너비
+    vp.Height = (FLOAT)height;  // 뷰포트 높이
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    vp.TopLeftX = 0; 	        // 그리기 시작 원점 x
+    vp.TopLeftY = 0; 	        // 그리기 시작 원점 y
+    g_pImmediateContext->RSSetViewports(1, &vp);
+
+
+    // Compile the vertex shader
+    ID3DBlob* pVSBlob = NULL;
+    hr = CompileShaderFromFile(L"BoxRender.fx", "VS", "vs_4_0", &pVSBlob);
+    if (FAILED(hr))
+    {
+        MessageBox(NULL,
+            L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+        return hr;
+    }
+
+    // Create the vertex shader
+    hr = g_pd3dDevice->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pVertexShader);
+    if (FAILED(hr))
+    {
+        pVSBlob->Release();
+        return hr;
+    }
+
+    // Define the input layout
+    D3D11_INPUT_ELEMENT_DESC layout[] =
+    {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    };
+    UINT numElements = ARRAYSIZE(layout);
+
+    // Create the input layout
+    hr = g_pd3dDevice->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(),
+        pVSBlob->GetBufferSize(), &g_pVertexLayout);
+    pVSBlob->Release();
+    if (FAILED(hr))
+        return hr;
+
+    // Set the input layout
+    g_pImmediateContext->IASetInputLayout(g_pVertexLayout);
+
+    // Compile the pixel shader
+    ID3DBlob* pPSBlob = NULL;
+    hr = CompileShaderFromFile(L"BoxRender.fx", "PS", "ps_4_0", &pPSBlob);
+    if (FAILED(hr))
+    {
+        MessageBox(NULL,
+            L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+        return hr;
+    }
+
+    // Create the pixel shader
+    hr = g_pd3dDevice->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), NULL, &g_pPixelShader);
+    pPSBlob->Release();
+    if (FAILED(hr))
+        return hr;
+
+    // Create vertex buffer
+    SimpleVertex vertices[] =
+    {
+        { XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+        { XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT4(0.0f, 1.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) },
+        { XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f) },
+        { XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f) },
+    };
+    D3D11_BUFFER_DESC bd;
+    ZeroMemory(&bd, sizeof(bd));
+    bd.Usage = D3D11_USAGE_DEFAULT;
+    bd.ByteWidth = sizeof(SimpleVertex)* 8;
+    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    bd.CPUAccessFlags = 0;
+    D3D11_SUBRESOURCE_DATA InitData;
+    ZeroMemory(&InitData, sizeof(InitData));
+    InitData.pSysMem = vertices;
+    hr = g_pd3dDevice->CreateBuffer(&bd, &InitData, &g_pVertexBuffer);
+    if (FAILED(hr))
+        return hr;
+
+    // Set vertex buffer
+    UINT stride = sizeof(SimpleVertex);
+    UINT offset = 0;
+    g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+    // Create index buffer
+    WORD indices[] =
+    {
+        3, 1, 0,
+        2, 1, 3,
+
+        0, 5, 4,
+        1, 5, 0,
+
+        3, 4, 7,
+        0, 4, 3,
+
+        1, 6, 5,
+        2, 6, 1,
+
+        2, 7, 6,
+        3, 7, 2,
+
+        6, 4, 5,
+        7, 4, 6,
+    };
+    bd.Usage = D3D11_USAGE_DEFAULT;
+    bd.ByteWidth = sizeof(WORD)* 36;        // 36 vertices needed for 12 triangles in a triangle list
+    bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    bd.CPUAccessFlags = 0;
+    InitData.pSysMem = indices;
+    hr = g_pd3dDevice->CreateBuffer(&bd, &InitData, &g_pIndexBuffer);
+    if (FAILED(hr))
+        return hr;
+
+    // Set index buffer
+    g_pImmediateContext->IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+    // Set primitive topology
+    g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+    // Create the constant buffer
+    bd.Usage = D3D11_USAGE_DEFAULT;
+    bd.ByteWidth = sizeof(ConstantBuffer);
+    bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+    bd.CPUAccessFlags = 0;
+    hr = g_pd3dDevice->CreateBuffer(&bd, NULL, &g_pConstantBuffer);
+    if (FAILED(hr))
+        return hr;
+
+    // Initialize the world matrix
+    g_World1 = XMMatrixIdentity();
+    g_World2 = XMMatrixIdentity();
+
+    // Initialize the view matrix
+    XMVECTOR Eye = XMVectorSet(0.0f, 1.0f, -5.0f, 0.0f);
+    XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    g_View = XMMatrixLookAtLH(Eye, At, Up);
+
+    // Initialize the projection matrix
+    g_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV2, width / (FLOAT)height, 0.01f, 100.0f);
+
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// 생성했던 오브젝트들 릴리즈
+//--------------------------------------------------------------------------------------
+void CleanupDevice()
+{
+    if (g_pImmediateContext) g_pImmediateContext->ClearState();
+
+    if (g_pConstantBuffer) g_pConstantBuffer->Release();
+    if (g_pVertexBuffer) g_pVertexBuffer->Release();
+    if (g_pIndexBuffer) g_pIndexBuffer->Release();
+    if (g_pVertexLayout) g_pVertexLayout->Release();
+    if (g_pVertexShader) g_pVertexShader->Release();
+    if (g_pPixelShader) g_pPixelShader->Release();
+    if (g_pDepthStencil) g_pDepthStencil->Release();
+    if (g_pDepthStencilView) g_pDepthStencilView->Release();
+    if (g_pRenderTargetView) g_pRenderTargetView->Release();
+    if (g_pSwapChain) g_pSwapChain->Release();
+    if (g_pImmediateContext) g_pImmediateContext->Release();
+    if (g_pd3dDevice) g_pd3dDevice->Release();
+}
+
+
+//--------------------------------------------------------------------------------------
+// 렌더링
+//--------------------------------------------------------------------------------------
+void Render()
+{
+    // Update our time
+    static float t = 0.0f;
+    if (g_driverType == D3D_DRIVER_TYPE_REFERENCE)
+    {
+        t += (float)XM_PI * 0.0125f;
+    }
+    else
+    {
+        static DWORD dwTimeStart = 0;
+        DWORD dwTimeCur = GetTickCount();
+        if (dwTimeStart == 0)
+            dwTimeStart = dwTimeCur;
+        t = (dwTimeCur - dwTimeStart) / 1000.0f;
+    }
+
+    // Cube: Rotate around the origin
+    g_World1 = XMMatrixRotationY(t) * XMMatrixRotationX(t);
+
+    //
+    // Clear the back buffer
+    //
+    float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f }; //red, green, blue, alpha
+    g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+
+    //
+    // Clear the depth buffer to 1.0 (max depth)
+    //
+    g_pImmediateContext->ClearDepthStencilView(g_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+    //
+    // Update variables for the cube
+    //
+    ConstantBuffer cb1;
+    cb1.mWorld = XMMatrixTranspose(g_World1);
+    cb1.mView = XMMatrixTranspose(g_View);
+    cb1.mProjection = XMMatrixTranspose(g_Projection);
+    g_pImmediateContext->UpdateSubresource(g_pConstantBuffer, 0, NULL, &cb1, 0, 0);
+
+    //
+    // Render the cube
+    //
+    g_pImmediateContext->VSSetShader(g_pVertexShader, NULL, 0);
+    g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+    g_pImmediateContext->PSSetShader(g_pPixelShader, NULL, 0);
+    g_pImmediateContext->DrawIndexed(36, 0, 0);
+
+    //
+    // Present our back buffer to our front buffer
+    //
+    g_pSwapChain->Present(0, 0); // 첫번째 인자 : 갱신 딜레이
+}
